c37ca087fcb33be5c9fd8733eeae347e2044516e
Stephen McDonald <steve@jupo.org>
2013-07-07 13:33:18 +1000

    Bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "django-postgres-fuzzycount",
-    version = "0.1.0",
+    version = "0.1.1",
     author = "Stephen McDonald",
     author_email = "stephen.mc@gmail.com",
     description = ("A Django model manager providing fast / fuzzy counts "
